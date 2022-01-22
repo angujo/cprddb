3ab53e301ae2665bc975e2e77ca10c47a0c9a041
angujo <angujomondi@gmail.com>
2022-01-22 12:00:43 +0000

    add_in table added

--- Scripts/stem_table.sql
+++ Scripts/stem_table.sql
@@ -1,42 +1,54 @@
 WITH voccur AS (SELECT * FROM visit_occurance),
 	ssource AS (SELECT * FROM source_to_source),
 	sstandard AS (SELECT * FROM source_to_standard),
+	concs AS (SELECT concept_id, domain_id, concept_name FROM concept WHERE standard_concept = 'S' and invalid_reason is NULL and (domain_id IN ('Meas Value','Meas Value Operator') OR vocabulary_id IN ('UCUM')) GROUP BY concept_code),
+	meds AS (SELECT * FROM medical),
 	clinicals AS (
 		SELECT
 		CASE WHEN st.source_concept_id IS NULL OR 0 = st.source_concept_id THEN 'Observation' else st.source_domain_id end, 
-		c.patid, vo.visit_occurrence_id, c.staffid, c.eventdate::timestamp, st.source_concept_id, c.medcode, ss.source_concept_id, 32827,NULL,NULL,NULL,c.eventdate, null
+		c.patid, vo.visit_occurrence_id, c.staffid, c.eventdate::timestamp start_datetime, m.read_code source_value, st.source_concept_id, c.medcode, ss.source_concept_id, 32827,NULL,NULL,NULL,c.eventdate, null
 		FROM clinical c
+		JOIN meds m AS c.medcode = m.medcode
 		JOIN vo ON vo.person_id = c.patid AND vo.visit_start_date = c.eventdate
-		LEFT JOIN ss ss ON ss.source_code = c.medcode AND ss.source_vocabulary_id = 'Read'
-		LEFT JOIN st st ON st.source_code = c.medcode AND st.source_vocabulary_id = 'Read' AND st.Target_standard_concept = 'S' and st.target_invalid_reason is NULL
+		LEFT JOIN ss ss ON ss.source_code = m.read_code AND ss.source_vocabulary_id = 'Read'
+		LEFT JOIN st st ON st.source_code = m.read_code AND st.source_vocabulary_id = 'Read' AND st.Target_standard_concept = 'S' and st.target_invalid_reason is NULL
 	),
 	referrals AS (
 		SELECT
 		CASE WHEN st.source_concept_id IS NULL OR 0 = st.source_concept_id THEN 'Observation' else st.source_domain_id end, 
-		r.patid, vo.visit_occurrence_id, r.staffid, r.eventdate::timestamp, st.source_concept_id, r.medcode, ss.source_concept_id, 32842,NULL,NULL,NULL,r.eventdate, null
+		r.patid, vo.visit_occurrence_id, r.staffid, r.eventdate::timestamp start_datetime, m.read_code source_value, st.source_concept_id, r.medcode, ss.source_concept_id, 32842,NULL,NULL,NULL,r.eventdate, null
 		FROM referral r
+		JOIN meds m AS r.medcode = m.medcode
 		JOIN voccur vo ON vo.person_id = c.patid AND vo.visit_start_date = r.eventdate
-		LEFT JOIN ssource ss ON ss.source_code = r.medcode AND ss.source_vocabulary_id = 'Read'
-		LEFT JOIN sstandard st ON st.source_code = r.medcode AND st.source_vocabulary_id = 'Read' AND st.Target_standard_concept = 'S' and st.target_invalid_reason is NULL
+		LEFT JOIN ssource ss ON ss.source_code = m.read_code AND ss.source_vocabulary_id = 'Read'
+		LEFT JOIN sstandard st ON st.source_code = m.read_code AND st.source_vocabulary_id = 'Read' AND st.Target_standard_concept = 'S' and st.target_invalid_reason is NULL
 	),
 	immunes AS (
 		SELECT
 		CASE WHEN st.source_concept_id IS NULL OR 0 = st.source_concept_id THEN 'Observation' else st.source_domain_id end, 
-		r.patid, vo.visit_occurrence_id, r.staffid, r.eventdate::timestamp, st.source_concept_id, r.medcode, ss.source_concept_id, 32827,NULL,NULL,NULL,r.eventdate, null
+		r.patid, vo.visit_occurrence_id, r.staffid, r.eventdate::timestamp start_datetime, m.read_code source_value, st.source_concept_id, r.medcode, ss.source_concept_id, 32827,NULL,NULL,NULL,r.eventdate, null
 		FROM immunisation r
+		JOIN meds m AS r.medcode = m.medcode
 		JOIN voccur vo ON vo.person_id = c.patid AND vo.visit_start_date = r.eventdate
-		LEFT JOIN ssource ss ON ss.source_code = r.medcode AND ss.source_vocabulary_id = 'Read'
-		LEFT JOIN sstandard st ON st.source_code = r.medcode AND st.source_vocabulary_id = 'Read' AND st.Target_standard_concept = 'S' and st.target_invalid_reason is NULL
+		LEFT JOIN ssource ss ON ss.source_code = m.read_code AND ss.source_vocabulary_id = 'Read'
+		LEFT JOIN sstandard st ON st.source_code = m.read_code AND st.source_vocabulary_id = 'Read' AND st.Target_standard_concept = 'S' and st.target_invalid_reason is NULL
 	),
 	tests AS (
 		SELECT
-		CASE WHEN st.source_concept_id IS NULL OR 0 = st.source_concept_id THEN 'Observation' else st.source_domain_id end, 
-		r.patid, vo.visit_occurrence_id, r.staffid, r.eventdate::timestamp, st.source_concept_id, r.medcode, ss.source_concept_id, 32856,NULL,NULL,NULL,r.eventdate, null
-		FROM test r
-		JOIN test_int ti ON ti.enttype = r.enttype
+		CASE WHEN st.source_concept_id IS NULL OR 0 = st.source_concept_id THEN 'Observation' else st.source_domain_id END AS domain_id, 
+		ti.patid AS person_id, vo.visit_occurrence_id, ti.staffid AS provider_id, ti.eventdate::timestamp AS start_datetime, ti.read_code source_value, st.source_concept_id AS concept_id, 
+		ss.source_concept_id, 32856 AS type_concept_id, 
+		CASE cop.concept_id IS NULL THEN 0 ELSE cop.concept_id end operator_concept_id,
+		cu.concept_id unit_concept_id,ti.unit unit_source_value,ti.eventdate start_date, 
+		null end_date, NULL AS sig, ti.range_high, ti.range_low, ti.value_as_number, cv.concept_id value_as_concept_id, ti.value_as_concept_id
+		FROM test_int ti
 		JOIN voccur vo ON vo.person_id = c.patid AND vo.visit_start_date = ti.eventdate
-		LEFT JOIN ssource ss ON ss.source_code = r.medcode AND ss.source_vocabulary_id = 'Read'
-		LEFT JOIN sstandard st ON st.source_code = r.medcode AND st.source_vocabulary_id = 'JNJ_CPRD_TEST_ENT' AND st.Target_standard_concept = 'S' and st.target_invalid_reason is NULL
+		LEFT JOIN concs cop ON cop.ti.operator = cop.concept_name AND cop.domain_id IN ('Meas Value Operator')
+		LEFT JOIN ssource ss ON ss.source_code = ti.read_code AND ss.source_vocabulary_id = 'Read'
+		LEFT JOIN sstandard st ON st.source_code = ti.read_code AND st.source_vocabulary_id = 'JNJ_CPRD_TEST_ENT' AND st.Target_standard_concept = 'S' and st.target_invalid_reason is NULL
+		LEFT JOIN concs cu ON cu.concept_code = ti.unit AND cu.vocabulary_id = 'UCUM'
+		LEFT JOIN concept cv ON cv.concept_name = ti.value_as_concept_id AND cc.domain_id IN ('Meas Value')
 	)
 INSERT INTO stem_table
-(domain_id, person_id, visit_occurrence_id, provider_id, start_datetime, concept_id, source_concept_id, type_concept_id, operator_concept_id, unit_concept_id, unit_source_value, start_date, end_date, sig)
+(domain_id, person_id, visit_occurrence_id, provider_id, start_datetime, concept_id, source_concept_id, type_concept_id, operator_concept_id, unit_concept_id, unit_source_value,
+start_date, end_date, sig, range_high, range_low, value_as_number, value_as_concept_id, value_source_value)
--- Scripts/test_int.sql
+++ Scripts/test_int.sql
@@ -4,50 +4,50 @@ DROP TABLE IF EXISTS test_int;
 -- create the table
 CREATE TABLE test_int AS (
 	WITH 
-		test_t AS (SELECT patid, eventdate, constype, consid, staffid, enttype, medcode, data1, data2, data3, data4, data5, data6 FROM SOURCE.test),
-		meds AS (SELECT read_code, description, medcode FROM SOURCE.medical),
-		entities AS (SELECT code, description, data_fields FROM SOURCE.entity WHERE data_fields in (7,8,4)),
+		test_t AS (SELECT patid, eventdate, consid, staffid, enttype, medcode, data1, data2, data3, data4, data5, data6 FROM SOURCE.test),
+		meds AS (SELECT read_code,"desc" description, medcode FROM SOURCE.medical),
+		entities AS (SELECT enttype, description, data_fields FROM SOURCE.entity WHERE data_fields in (7,8,4)),
 		lookups AS (SELECT code, lookup_type_id, text FROM SOURCE.lookup WHERE lookup_type_id IN (56,83,85)) 
 	SELECT 
 	       tt.patid,
 	       tt.eventdate,
-	       tt.constype,
 	       tt.consid,
 	       tt.staffid,
 	       mc.read_code,
+	       tt.medcode,
 	       mc.description as read_description,
-	       CAST(e.code as varchar)||'-'||e.description as map_value,       
-	       et.code as enttype,
+	       CAST(et.enttype as varchar)||'-'||et.description as map_value,       
+	       et.enttype,
 	       et.description as enttype_desc,
 	       et.data_fields,
-	       case when tt.data1 <> 0 --In this case 0 means 'data not entered' so it is set to blank
+	       case when tt.data1 <> '0' --In this case 0 means 'data not entered' so it is set to blank
 	        then lp.text else '' end as operator, --Data1 is the coded operator (<,>,=,>=,<=). The join on line 29 looks up the text value and stores that here. 
 	       tt.data2 as value_as_number,
-	       case when tt.data3 <> 0 then lp2.text --Here the number 0 means 'data not entered' so it is set to blank
-	         when et.code = 284 and (t.data2 is not null and tt.data2 <> 0) then 'week' --Enttype (code) 284 refers to gestational age in weeks so the unit is hard coded
+	       case when tt.data3 <> '0' then lp2.text --Here the number 0 means 'data not entered' so it is set to blank
+	         when et.enttype = 284 and (tt.data2 is not null and tt.data2 <> '0') then 'week' --Enttype (code) 284 refers to gestational age in weeks so the unit is hard coded
 	         else '' end as unit, --Data3 is the coded unit of the laboratory test. The join on line 32 looks up the text value and stores that here.
-	       case when tt.data4 <> 0 then lp3.text else '' end as value_as_concept_id, --In this case 0 means 'data not entered' so it is set to blank. Data4 is the coded qualifier (high,low). The join on line 35 looks up the text value and stores that value here.
+	       case when tt.data4 <> '0' then lp3.text else '' end as value_as_concept_id, --In this case 0 means 'data not entered' so it is set to blank. Data4 is the coded qualifier (high,low). The join on line 35 looks up the text value and stores that value here.
 	       tt.data5 as range_low,
 	       tt.data6 as range_high     
 	FROM test_t tt
-	JOIN entities et ON tt.enttype = et.code
+	JOIN entities et ON tt.enttype = et.enttype
 	JOIN meds mc ON tt.medcode = mc.medcode
-	JOIN lookups lp ON tt.data1 = lp.code AND lp.lookup_type_id = 56 /*OPR - Operator*/
-	JOIN lookups lp2 ON tt.data3 = lp2.code AND lp2.lookup_type_id = 83 /*SUM - Specimen Unit of Measure*/
-	JOIN lookups lp3 ON tt.data4 = lp3.code AND lp3.lookup_type_id = 85 /*TQU - Test Qualifier*/ 
+	JOIN lookups lp ON tt.data1 = lp.code::text AND lp.lookup_type_id = 56 /*OPR - Operator*/
+	JOIN lookups lp2 ON tt.data3 = lp2.code::text AND lp2.lookup_type_id = 83 /*SUM - Specimen Unit of Measure*/
+	JOIN lookups lp3 ON tt.data4 = lp3.code::text AND lp3.lookup_type_id = 85 /*TQU - Test Qualifier*/ 
 	WHERE et.data_fields in (7,8) --When data_fields equals 7 or 8 then both operators and units may be present. 
 	
 	UNION
 	
 	SELECT tt.patid,
 	       tt.eventdate,
-	       tt.constype,
 	       tt.consid,
 	       tt.staffid,
 	       mc.read_code,
+	       tt.medcode,
 	       mc.description as read_description,
-	       CAST(e.code as varchar)||'-'||e.description as map_value,
-	       et.code as enttype,
+	       CAST(et.enttype as varchar)||'-'||et.description as map_value,
+	       et.enttype,
 	       et.description as enttype_desc,
 	       et.data_fields,
 	       '' as operator,
@@ -57,8 +57,8 @@ CREATE TABLE test_int AS (
 	       tt.data2 as range_low,
 	       tt.data3 as range_high     
 	FROM test_t tt
-	JOIN entities et ON tt.enttype = et.code
+	JOIN entities et ON tt.enttype = et.enttype
 	JOIN meds mc ON tt.medcode = mc.medcode
-	JOIN lookups lp ON tt.data1 = lp.code AND lp.lookup_type_id = 85 /*TQU - Test Qualifier*/ 
+	JOIN lookups lp ON tt.data1 = lp.code::text AND lp.lookup_type_id = 85 /*TQU - Test Qualifier*/ 
 	WHERE et.data_fields = 4 --When data_fields equals 4 then only the value of the text and qualifier is available.
 );
